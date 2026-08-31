f1284d186b6a29c10017e0922a3f43754709cbe6
Ifeoma <u.ifeoma22@gmail.com>
2026-08-31 12:12:08 +0100

    service logic for db queries analytics controller for https requests. calls service

--- src/main/java/com/rlcv/controller/AnalyticsController.java
+++ src/main/java/com/rlcv/controller/AnalyticsController.java
@@ -1,23 +1,188 @@
 package com.rlcv.controller;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.rlcv.dto.AnalyticsResponse;
+import com.rlcv.dto.EventTypeBreakdown;
+import com.rlcv.dto.HourlyBreakdown;
+import com.rlcv.dto.TopUrlResult;
+import com.rlcv.exceptions.AccessDeniedException;
+import com.rlcv.model.ApiKey;
+import com.rlcv.model.Event;
 import com.rlcv.service.AnalyticsService;
+import com.rlcv.service.ApiKeyService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/analytics")
+@RequiredArgsConstructor
+@Tag(name = "Analytics Endpoints", description = "Query for Analysis")
 public class AnalyticsController {
 
-    private AnalyticsService analyticsService;
+    private final AnalyticsService analyticsService;
+    private final ApiKeyService apiKeyService;
+    private Map<String, List<SseEmitter>> emittersByUrl = new ConcurrentHashMap<>();
 
-    @PostMapping
-    public ResponseEntity<AnalyticsResponse> getAnalytics(
-            @PathVariable String url, @RequestParam(required = false) String eventType, @RequestParam(required = false) LocalDate date
+    @GetMapping("/")
+    @Operation(summary = "Return all recorded events")
+    public ResponseEntity<Page<Event>> getRecordedEvents(
+        @RequestParam(defaultValue = "50") int size
     ) {
-        return ResponseEntity.ok(analyticsService.getAnalytics(url, eventType, date));
+        return ResponseEntity.ok(analyticsService.getAllEvent(size));
+    }
+
+    @GetMapping("/{url}")
+    @Operation(summary = "Get total count of visit to a url")
+    public ResponseEntity<AnalyticsResponse> getTotalCountOnAUrl(
+            @PathVariable String url, 
+            @RequestParam(required = false) LocalDate date,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+        
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        return ResponseEntity.ok(analyticsService.getTotalCountOnDomain(url, queryDate));
+    }
+
+    @GetMapping("/{url}/eventtype")
+    @Operation(summary = "Get counts of an event on a url")
+    public ResponseEntity<AnalyticsResponse> getTotalCountOfEventType(
+            @PathVariable String url,
+            @RequestParam String eventType,
+            @RequestParam(required = false) LocalDate date,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        return ResponseEntity.ok(analyticsService.getCountByEventType(url, eventType, queryDate));
+    }
+
+    @GetMapping("/{url}/breakdown")
+    @Operation(summary = "Get counts of all present events on a url")
+    public ResponseEntity<List<EventTypeBreakdown>> getTotalCountForEachEventBreakdown(
+            @PathVariable String url,
+            @RequestParam(required = false) LocalDate date,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        return ResponseEntity.ok(analyticsService.getEventypeBreakdown(url, queryDate));
+    }
+
+    @GetMapping("/top")
+    @Operation(summary = "Get list of top urls in descending order")
+    public ResponseEntity<List<TopUrlResult>> getTopUrls(
+            @RequestParam(required = false) LocalDate date
+    ) {
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        return ResponseEntity.ok(analyticsService.getTopUrls(queryDate));
+    }
+
+    @GetMapping("/{url}/hourly")
+    @Operation(summary = "Get a breakdown of hourly activities on a url")
+    public ResponseEntity<List<HourlyBreakdown>> getHourlyBreakdown(
+            @PathVariable String url,
+            @RequestParam(required = false) LocalDate date,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        return ResponseEntity.ok(analyticsService.getHourlyBreakdown(url, queryDate));
+    }
+
+    @GetMapping("/{url}/eventtype/details")
+    @Operation(summary = "Get the details of a returned event, time, id, url etc")
+    public ResponseEntity<List<Event>> getEventDetails(
+            @PathVariable String url,
+            @RequestParam String eventType,
+            @RequestParam(required = false) LocalDate date,
+            @RequestParam(required = false) LocalDateTime cursor,
+            @RequestParam(defaultValue = "50") int size,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        LocalDateTime startingCursor = cursor != null ? cursor : queryDate.atTime(LocalTime.MAX);
+        return ResponseEntity.ok(analyticsService.getEventDetails(url, eventType, queryDate, startingCursor, size));
+    }
+
+    @GetMapping("/{url}/details")
+    @Operation(summary = "Get the details of events for a url")
+    public ResponseEntity<Page<Event>> getDetailsOfAllEventsOfAUrl(
+            @PathVariable String url,
+            @RequestParam(required = false) LocalDate date,
+            @RequestParam(required = false) LocalDateTime cursor,
+            @RequestParam(defaultValue = "50") int size,
+            HttpServletRequest httpRequest
+    ) {
+        checkOwnership(httpRequest, url);
+
+        LocalDate queryDate = date != null ? date : LocalDate.now();
+        LocalDateTime startingCursor = cursor != null ? cursor : queryDate.atTime(LocalTime.MAX);
+        return ResponseEntity.ok(analyticsService.getEventDetailsByDomain(url, queryDate, startingCursor, size));
+    }
+
+    @Operation(summary = "Constant feed of the last 15 events in the db")
+    @GetMapping(value = "/feed", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter liveFeed(@RequestParam String url, HttpServletRequest httpRequest) {
+        
+        checkOwnership(httpRequest, url);
+        
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); 
+        emittersByUrl.computeIfAbsent(url, k -> new CopyOnWriteArrayList<>()).add(emitter);
+
+        // clean up when there is a disconnect
+        emitter.onCompletion(() -> emittersByUrl.get(url).remove(emitter));
+        emitter.onTimeout(() -> emittersByUrl.get(url).remove(emitter));
+
+        return emitter;
+    }
+
+    public void pushToFeed (Event event) {
+        List<SseEmitter> emitters = emittersByUrl.get(event.getUrl());
+        if (emitters == null) return;
+        
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(SseEmitter.event().data(event));
+            } catch (IOException e) {
+                emitters.remove(emitter);
+            }
+        }
+    }
+
+
+
+    private void checkOwnership(HttpServletRequest httpRequest, String url) {
+        ApiKey apiKey = (ApiKey) httpRequest.getAttribute("apiKey");
+
+        if (!apiKeyService.ownsUrl(apiKey, url)) {
+            throw new AccessDeniedException("You do not own this URL");
+        }
     }
 }
--- src/main/java/com/rlcv/service/AnalyticsService.java
+++ src/main/java/com/rlcv/service/AnalyticsService.java
@@ -1,11 +1,27 @@
 package com.rlcv.service;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rlcv.dto.AnalyticsResponse;
+import com.rlcv.dto.EventTypeBreakdown;
+import com.rlcv.dto.HourlyBreakdown;
+import com.rlcv.dto.TopUrlResult;
+import com.rlcv.model.Event;
 import com.rlcv.repository.EventRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,35 +30,194 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AnalyticsService {
 
-    private EventRepository eventRepository;
-    private RedisTemplate<String,String> redisTemplate;
+    private final EventRepository eventRepository;
+    private final RedisTemplate<String,String> redisTemplate;
+    private final ObjectMapper objectMapper;
 
-    public AnalyticsResponse getAnalytics(String url, String eventType, LocalDate date) {
-        LocalDate queryDate = date != null ? date : LocalDate.now();
+    // get all recoded events - used pageable to return 50 rows
+    public Page<Event> getAllEvent(int pageSize) {
+        Pageable pageable = PageRequest.of(0, pageSize, Sort.by("timestamp", "id").descending());
+        return eventRepository.findAllEvent(pageable);
+    }
 
-        // if the eventtype is not empty, query db
-        if (eventType != null) {
-            long count = eventRepository.countByUrlAndEventTypeAndDate(url, eventType, queryDate);
+    // return event details
+    public List<Event> getEventDetails(String url, String eventType, LocalDate date, LocalDateTime cursor, int pageSize) {
+        Pageable pageable = PageRequest.of(0, pageSize, Sort.by("timestamp", "id").descending());
+        return eventRepository.findByUrlStartingWithAndEventTypeAndTimestampBetweenAndTimestampLessThan(
+            url, eventType, date.atStartOfDay(), date.atTime(LocalTime.MAX), cursor, pageable);
+    }
+
+    // keep live feed of db changes
+    // might have to remove this
+    public List<Event> getLiveFeed(String url) {
+        return eventRepository.findTop15ByUrlOrderByTimestampDesc(url);
+    }
+
+    // get total count for a url on a date
+    // might have to remove this
+    public AnalyticsResponse getTotalCount(String url, LocalDate date) {
+        String cacheKey = CacheKeys.totalCount(url, date);
+
+        return getCachedOrQuery(cacheKey, () -> {
+            long count = eventRepository.countByUrlAndTimestampBetween(
+                url, date.atStartOfDay(), date.atTime(LocalTime.MAX));
+
+            return AnalyticsResponse.builder()
+                    .url(url)
+                    .count(count)
+                    .date(date.toString())
+                    .eventType("all_events")
+                    .build();
+        });
+    }
+
+    // get total count for a domain url
+    public AnalyticsResponse getTotalCountOnDomain(String urlPrefix, LocalDate date) {
+        String cacheKey = CacheKeys.totalCount(urlPrefix, date);
+
+        return getCachedOrQuery(cacheKey, () -> {
+            long count = eventRepository.countByUrlStartingWithAndTimestampBetween(
+                urlPrefix, date.atStartOfDay(), date.atTime(LocalTime.MAX));
+
+            return AnalyticsResponse.builder()
+                    .url(urlPrefix)
+                    .count(count)
+                    .date(date.toString())
+                    .eventType("all_events")
+                    .build();
+        });
+    }
+
+    //count by event type for a url on a date
+    public AnalyticsResponse getCountByEventType(String url, String eventType, LocalDate date) {
+        String cacheKey = CacheKeys.eventTypeQuery(url, eventType, date);
+
+        return getCachedOrQuery(cacheKey, () -> {
+            long count = eventRepository.countByUrlStartingWithAndEventTypeAndTimestampBetween(
+                url, eventType, date.atStartOfDay(), date.atTime(LocalTime.MAX));
+
             return AnalyticsResponse.builder()
                     .url(url)
                     .eventType(eventType)
                     .count(count)
-                    .date(queryDate.toString())
+                    .date(date.toString())
                     .build();
+        });
+    }
+
+    // breakdown by event type for a url on a date
+    public List<EventTypeBreakdown> getEventypeBreakdown(String url, LocalDate date) {
+        String cacheKey = CacheKeys.breakdown(url, date);
+        String cached =  redisTemplate.opsForValue().get(cacheKey);
+
+        if (cached != null) {
+            return deserializeList(cached, EventTypeBreakdown.class);
         }
 
-        // if event type is not added, check redis first
-        String counterKey = CacheKeys.counterKey(url, queryDate);
-        String cached = redisTemplate.opsForValue().get(counterKey);
+        List<Object[]> results = eventRepository.countUrlStartingWithAndGroupedByEventType(
+            url, date.atStartOfDay(), date.atTime(LocalTime.MAX));
 
-        long count = cached != null 
-                ? Long.parseLong(cached) 
-                : eventRepository.countByUrlAndDate(url, queryDate);
+        List<EventTypeBreakdown> breakdown = results.stream()
+            .map(row -> EventTypeBreakdown.builder()
+                    .eventType((String) row[0])
+                    .count((Long) row[1])
+                    .build())
+            .collect(Collectors.toList());
 
-        return AnalyticsResponse.builder()
-                .url(url)
-                .count(count)
-                .date(LocalDate.now().toString())
-                .build();
+        redisTemplate.opsForValue().set(cacheKey, serialize(breakdown), Duration.ofMinutes(5));
+        return breakdown;
     }
+
+    // top urls by event count on a date
+    public List<TopUrlResult> getTopUrls(LocalDate date) {
+        String cacheKey = CacheKeys.topUrls(date);
+        String cached = redisTemplate.opsForValue().get(cacheKey);
+
+        if (cached != null) {
+            return deserializeList(cached, TopUrlResult.class);
+        }
+
+        List<Object[]> results = eventRepository.findTopUrls(
+            date.atStartOfDay(), date.atTime(LocalTime.MAX));
+
+        List<TopUrlResult> topUrls = results.stream()
+            .map(row -> TopUrlResult.builder()
+                    .url((String) row[0])
+                    .count((Long) row[1])
+                    .build()
+                )
+            .collect(Collectors.toList());
+        
+        redisTemplate.opsForValue().set(cacheKey, serialize(topUrls), Duration.ofMinutes(5));
+        return topUrls;
+    }
+
+    // events per hour for a url on a date
+    public List<HourlyBreakdown> getHourlyBreakdown(String url, LocalDate date) {
+        String cacheKey = CacheKeys.hourly(url, date);
+        String cached = redisTemplate.opsForValue().get(cacheKey);
+
+        if (cached != null) {
+            return deserializeList(cached, HourlyBreakdown.class);
+        }
+
+        List<Object[]> results = eventRepository.countUrlStartingWithAndPerHour(
+            url, date.atStartOfDay(), date.atTime(LocalTime.MAX));
+
+        List<HourlyBreakdown> hourly = results.stream()
+            .map(row -> HourlyBreakdown.builder()
+                    .hour((int) row[0])
+                    .count((Long) row[1])
+                    .build()
+                )
+            .collect(Collectors.toList());
+        
+        redisTemplate.opsForValue().set(cacheKey, serialize(hourly), Duration.ofMinutes(5));
+        return hourly;
+    }
+
+    // return event details for a domain
+    public Page<Event> getEventDetailsByDomain(String url, LocalDate date, LocalDateTime cursor, int pageSize) {
+        Pageable pageable = PageRequest.of(0, pageSize, Sort.by("timestamp", "id").descending());
+        return eventRepository.findByUrlStartingWithAndTimestampBetweenAndTimestampLessThan(
+            url, date.atStartOfDay(), date.atTime(LocalTime.MAX), cursor, pageable);
+    }
+
+
+    // ##################### IN CLASS HELPER FUNCTIONS ############################
+
+    private AnalyticsResponse getCachedOrQuery(String cacheKey, Supplier<AnalyticsResponse> query) {
+        String cached = redisTemplate.opsForValue().get(cacheKey);
+        if (cached != null) {
+            return deserialize(cached, AnalyticsResponse.class);
+        }
+        AnalyticsResponse result = query.get();
+        redisTemplate.opsForValue().set(cacheKey, serialize(result), Duration.ofMinutes(5));
+        return result;
+    }
+
+    private String serialize(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Serialization failed", e);
+        }
+    }
+
+    private <T> T deserialize(String json, Class<T> type) {
+        try {
+            return objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Deserialization failed", e);
+        }
+    }
+
+    private <T> List<T> deserializeList(String json, Class<T> type) {
+        try {
+            return objectMapper.readValue(json, objectMapper.getTypeFactory().constructCollectionType(List.class, type));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("List Deserialization failed", e);
+        }
+    }
+
 }
